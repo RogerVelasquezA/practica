48e866a610dfb3c7bbb2b59a0859b25a9780ebca
rvelasquez <rogervelasquez666@gmail.com>
2022-04-18 13:49:53 -0500

    .net core

---
+++ appWeb01_T5GB01/Controllers/NegociosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace appWeb01_T5GB01.Controllers
+{
+    public class NegociosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ appWeb01_T5GB01/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace appWeb01_T5GB01.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
